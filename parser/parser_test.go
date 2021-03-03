e41b3b464eddcf103d2ef7c773d997b899f75429
Flipez <code@brauser.io>
2021-03-03 23:57:59 +0100

    add and print parser errors
    Signed-off-by: Flipez <code@brauser.io>

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -18,6 +18,7 @@ let foobar = 838383;
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
@@ -40,6 +41,19 @@ let foobar = 838383;
 			return
 		}
 	}
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
