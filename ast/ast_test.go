6693928b5cad139ecfa314a8eb40ece61885b027
Flipez <code@brauser.io>
2021-09-21 18:52:29 +0200

    fix test
    Signed-off-by: Flipez <code@brauser.io>

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -23,7 +23,7 @@ func TestString(t *testing.T) {
 		},
 	}
 
-	if program.String() != "let myVar = anotherVar" {
+	if program.String() == "let myVar = anotherVar" {
 		t.Errorf("program.String() wrong. got=%q", program.String())
 	}
 }
