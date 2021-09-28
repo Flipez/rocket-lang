1bcf34cba71a826c032bafec885d8cfb3758133e
Flipez <code@brauser.io>
2021-09-28 21:07:41 +0200

    add basic ability to parse files
    Signed-off-by: Flipez <code@brauser.io>

--- main.go
+++ main.go
@@ -1,11 +1,45 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 
+	"github.com/flipez/rocket-lang/evaluator"
+	"github.com/flipez/rocket-lang/lexer"
+	"github.com/flipez/rocket-lang/object"
+	"github.com/flipez/rocket-lang/parser"
 	"github.com/flipez/rocket-lang/repl"
 )
 
 func main() {
-	repl.Start(os.Stdin, os.Stdout)
+	if len(os.Args) == 1 {
+		repl.Start(os.Stdin, os.Stdout)
+	} else {
+		file, err := ioutil.ReadFile(os.Args[1])
+		if err == nil {
+			env := object.NewEnvironment()
+			l := lexer.New(string(file))
+			p := parser.New(l)
+
+			program := p.ParseProgram()
+			if len(p.Errors()) > 0 {
+				printParserErrors(p.Errors())
+				return
+			}
+
+			evaluated := evaluator.Eval(program, env)
+			if evaluated != nil {
+				fmt.Println(evaluated.Inspect())
+			}
+		}
+	}
+}
+
+func printParserErrors(errors []string) {
+	fmt.Println("🔥 Great, you broke it!")
+	fmt.Println(" parser errors:")
+	for _, msg := range errors {
+		fmt.Printf("\t %s\n", msg)
+	}
 }
