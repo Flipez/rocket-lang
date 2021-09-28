149f7a7eef22cdefde6194b1a295331c00c418f6
Flipez <code@brauser.io>
2021-09-28 22:10:15 +0200

    add pop() builtin
    Signed-off-by: Flipez <code@brauser.io>

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -96,6 +96,29 @@ var builtins = map[string]*object.Builtin{
 			return &object.Array{Elements: newElements}
 		},
 	},
+	"pop": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `last` must be ARRAY, got=%s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+
+			newElements := make([]object.Object, length-1, length-1)
+			copy(newElements, arr.Elements[:(length-1)])
+
+			returnArray := make([]object.Object, 2)
+
+			returnArray[0] = &object.Array{Elements: newElements}
+			returnArray[1] = arr.Elements[length-1]
+
+			return &object.Array{Elements: returnArray}
+		},
+	},
 	"puts": &object.Builtin{
 		Fn: func(args ...object.Object) object.Object {
 			for _, arg := range args {
