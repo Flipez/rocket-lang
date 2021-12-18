bea12ada68ae3caf31e980308fccc772d5515cde
Flipez <code@brauser.io>
2021-12-18 22:03:21 +0100

    add support for array yeet and yoink
    Signed-off-by: Flipez <code@brauser.io>

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -6,6 +6,50 @@ import (
 	"os"
 )
 
+var array_pop = &object.Builtin{
+	Fn: func(args ...object.Object) object.Object {
+		if len(args) != 1 {
+			return newError("wrong number of arguments. got=%d, want=1", len(args))
+		}
+		if args[0].Type() != object.ARRAY_OBJ {
+			return newError("argument to `last` must be ARRAY, got=%s", args[0].Type())
+		}
+
+		arr := args[0].(*object.Array)
+		length := len(arr.Elements)
+
+		newElements := make([]object.Object, length-1, length-1)
+		copy(newElements, arr.Elements[:(length-1)])
+
+		returnArray := make([]object.Object, 2)
+
+		returnArray[0] = &object.Array{Elements: newElements}
+		returnArray[1] = arr.Elements[length-1]
+
+		return &object.Array{Elements: returnArray}
+	},
+}
+
+var array_push = &object.Builtin{
+	Fn: func(args ...object.Object) object.Object {
+		if len(args) != 2 {
+			return newError("wrong number of arguments. got=%d, want=2", len(args))
+		}
+		if args[0].Type() != object.ARRAY_OBJ {
+			return newError("argument to `last` must be ARRAY, got=%s", args[0].Type())
+		}
+
+		arr := args[0].(*object.Array)
+		length := len(arr.Elements)
+
+		newElements := make([]object.Object, length+1, length+1)
+		copy(newElements, arr.Elements)
+		newElements[length] = args[1]
+
+		return &object.Array{Elements: newElements}
+	},
+}
+
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
 		Fn: func(args ...object.Object) object.Object {
@@ -78,48 +122,10 @@ var builtins = map[string]*object.Builtin{
 			return NULL
 		},
 	},
-	"push": &object.Builtin{
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 2 {
-				return newError("wrong number of arguments. got=%d, want=2", len(args))
-			}
-			if args[0].Type() != object.ARRAY_OBJ {
-				return newError("argument to `last` must be ARRAY, got=%s", args[0].Type())
-			}
-
-			arr := args[0].(*object.Array)
-			length := len(arr.Elements)
-
-			newElements := make([]object.Object, length+1, length+1)
-			copy(newElements, arr.Elements)
-			newElements[length] = args[1]
-
-			return &object.Array{Elements: newElements}
-		},
-	},
-	"pop": &object.Builtin{
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. got=%d, want=1", len(args))
-			}
-			if args[0].Type() != object.ARRAY_OBJ {
-				return newError("argument to `last` must be ARRAY, got=%s", args[0].Type())
-			}
-
-			arr := args[0].(*object.Array)
-			length := len(arr.Elements)
-
-			newElements := make([]object.Object, length-1, length-1)
-			copy(newElements, arr.Elements[:(length-1)])
-
-			returnArray := make([]object.Object, 2)
-
-			returnArray[0] = &object.Array{Elements: newElements}
-			returnArray[1] = arr.Elements[length-1]
-
-			return &object.Array{Elements: returnArray}
-		},
-	},
+	"push":  array_push,
+	"yoink": array_push,
+	"pop":   array_pop,
+	"yeet":  array_pop,
 	"puts": &object.Builtin{
 		Fn: func(args ...object.Object) object.Object {
 			for _, arg := range args {
