64ea4b16adf7e7d76ed1d56d37e5f808d678b7b0
Flipez <code@brauser.io>
2021-12-18 22:28:56 +0100

    move array to own file
    Signed-off-by: Flipez <code@brauser.io>

--- object/object.go
+++ object/object.go
@@ -84,27 +84,6 @@ func (i *Integer) HashKey() HashKey {
 	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
 }
 
-type Array struct {
-	Elements []Object
-}
-
-func (ao *Array) Type() ObjectType { return ARRAY_OBJ }
-func (ao *Array) Inspect() string {
-	var out bytes.Buffer
-
-	elements := []string{}
-	for _, e := range ao.Elements {
-		elements = append(elements, e.Inspect())
-	}
-
-	out.WriteString("[")
-	out.WriteString(strings.Join(elements, ", "))
-	out.WriteString("]")
-
-	return out.String()
-}
-func (ao *Array) InvokeMethod(method string, env Environment, args ...Object) Object { return nil }
-
 type BuiltinFunction func(args ...Object) Object
 
 type Builtin struct {
