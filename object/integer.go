8b00288231ecd8ea6824d3be25ca6f0625fa0eb0
Flipez <code@brauser.io>
2021-12-19 22:40:57 +0100

    convert integer methods to new style
    Signed-off-by: Flipez <code@brauser.io>

--- object/integer.go
+++ object/integer.go
@@ -14,10 +14,42 @@ func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 func (i *Integer) HashKey() HashKey {
 	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
 }
+
+var integerObjectMethods = map[string]ObjectMethod{
+	"type": ObjectMethod{
+		method: func(o Object, _ []Object) Object {
+			return &String{Value: string(o.Type())}
+		},
+	},
+	"plz_s": ObjectMethod{
+		argsOptional: true,
+		argPattern: [][]string{
+			[]string{INTEGER_OBJ},
+		},
+		method: func(o Object, args []Object) Object {
+			i := o.(*Integer)
+
+			base := 10
+			if len(args) > 0 {
+				base = int(args[0].(*Integer).Value)
+			}
+
+			return &String{Value: strconv.FormatInt(i.Value, base)}
+		},
+	},
+}
+
 func (i *Integer) InvokeMethod(method string, env Environment, args ...Object) Object {
 	switch method {
-	case "plz_s":
-		return &String{Value: strconv.FormatInt(i.Value, 10)}
+	case "methods":
+		return listObjectMethods(integerObjectMethods)
+	case "wat":
+		return listObjectUsage(i, integerObjectMethods)
+	default:
+		if objMethod, ok := integerObjectMethods[method]; ok {
+			return objMethod.Call(i, args)
+		}
 	}
+
 	return nil
 }
--- object/integer_test.go
+++ object/integer_test.go
@@ -8,7 +8,9 @@ import (
 func TestIntegerObjectMethods(t *testing.T) {
 	tests := []inputTestCase{
 		{`2.plz_s()`, "2"},
+		{`10.plz_s(2)`, "1010"},
 		{`2.nope()`, "Failed to invoke method: nope"},
+		{`(2.wat().lines().size() == 2.methods().size() + 1).plz_s()`, "true"},
 	}
 
 	testInput(t, tests)
