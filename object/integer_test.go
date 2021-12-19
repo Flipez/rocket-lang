afaa4db81593ce8ce53f709c1e91b3e60cb3c066
Flipez <code@brauser.io>
2021-12-19 15:55:30 +0100

    add tests for integer methods
    Signed-off-by: Flipez <code@brauser.io>

---
+++ object/integer_test.go
@@ -0,0 +1,37 @@
+package object_test
+
+import (
+	"github.com/flipez/rocket-lang/object"
+	"testing"
+)
+
+func TestIntegerObjectMethods(t *testing.T) {
+	tests := []inputTestCase{
+		{`2.plz_s()`, "2"},
+		{`2.nope()`, "Failed to invoke method: nope"},
+	}
+
+	testInput(t, tests)
+}
+
+func TestIntegerHashKey(t *testing.T) {
+	int1_1 := &object.Integer{Value: 1}
+	int1_2 := &object.Integer{Value: 1}
+	int2 := &object.Integer{Value: 2}
+
+	if int1_1.HashKey() != int1_2.HashKey() {
+		t.Errorf("integer with same content have different hash keys")
+	}
+
+	if int1_1.HashKey() == int2.HashKey() {
+		t.Errorf("integer with different content have same hash keys")
+	}
+}
+
+func TestIntegerInspect(t *testing.T) {
+	int1 := &object.Integer{Value: 1}
+
+	if int1.Inspect() != "1" {
+		t.Errorf("integer inspect does not match value")
+	}
+}
