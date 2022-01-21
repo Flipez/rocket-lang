d9377ff17b7609d71a7c18b6cdae94f6df4a8c4d
Robert Müller <code@brauser.io>
2022-01-21 22:07:11 +0100

    add cli flags (#57)
    * add cli flags
    
    Signed-off-by: Flipez <code@brauser.io>
    
    * improve cli args
    
    Signed-off-by: Flipez <code@brauser.io>
    
    * update doc
    
    Signed-off-by: Flipez <code@brauser.io>
    
    * improve usage
    
    Signed-off-by: Flipez <code@brauser.io>
    
    * update docs
    
    Signed-off-by: Flipez <code@brauser.io>

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"os"
 
+	flag "github.com/spf13/pflag"
+
 	"github.com/flipez/rocket-lang/evaluator"
 	"github.com/flipez/rocket-lang/lexer"
 	"github.com/flipez/rocket-lang/object"
@@ -13,26 +15,51 @@ import (
 )
 
 func main() {
+	version := flag.BoolP("version", "v", false, "Prints the version and build date.")
+	exec := flag.StringP("exec", "e", "", "Runs the given code.")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: rocket-lang [flags] [program file] [arguments]\n\nAvailable flags:\n")
+
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	if *version {
+		print(repl.SplashVersion())
+		return
+	}
+
+	if len(*exec) > 0 {
+		runProgram(*exec)
+		return
+	}
+
 	if len(os.Args) == 1 {
 		repl.Start(os.Stdin, os.Stdout)
 	} else {
 		file, err := ioutil.ReadFile(os.Args[1])
 		if err == nil {
-			env := object.NewEnvironment()
-			l := lexer.New(string(file))
-			p := parser.New(l, make(map[string]struct{}))
-
-			program, _ := p.ParseProgram()
-			if len(p.Errors()) > 0 {
-				printParserErrors(p.Errors())
-				return
-			}
-
-			evaluated := evaluator.Eval(program, env)
-			if evaluated != nil {
-				fmt.Println(evaluated.Inspect())
-			}
+			runProgram(string(file))
 		}
+	}
+}
+
+func runProgram(input string) {
+	env := object.NewEnvironment()
+	l := lexer.New(input)
+	p := parser.New(l, make(map[string]struct{}))
+
+	program, _ := p.ParseProgram()
+	if len(p.Errors()) > 0 {
+		printParserErrors(p.Errors())
+		return
+	}
+
+	evaluated := evaluator.Eval(program, env)
+	if evaluated != nil {
+		fmt.Println(evaluated.Inspect())
 	}
 }
 
