7c7cfbf834b0127d86a937501ecfc9836cc98c53
Flipez <code@brauser.io>
2022-02-24 21:23:02 +0100

    add examples to docs
    Signed-off-by: Flipez <code@brauser.io>

--- docs/generate.go
+++ docs/generate.go
@@ -33,20 +33,81 @@ func main() {
 b = "test" + "_string";
 
 is_true = "test" == "test";
-is_false = "test" == "string";`,
+is_false = "test" == "string";
+
+s = "abcdef"
+puts(s[2])
+puts(s[-2])
+puts(s[:2])
+puts(s[:-2])
+puts(s[2:])
+puts(s[-2:])
+puts(s[1:-2])
+
+s[2] = "C"
+s[-2] = "E"
+puts(s)
+
+// should output
+"c"
+"e"
+"ab"
+"abcd"
+"cdef"
+"ef"
+"bcd"
+"abCdEf"`,
 		LiteralMethods: string_methods,
 		DefaultMethods: default_methods}
 	create_doc("docs/templates/literal.md", "docs/content/docs/literals/string.md", tempData)
 
 	tempData = templateData{
-		Title:          "Array",
+		Title: "Array",
+		Example: `a = [1, 2, 3, 4, 5]
+puts(a[2])
+puts(a[-2])
+puts(a[:2])
+puts(a[:-2])
+puts(a[2:])
+puts(a[-2:])
+puts(a[1:-2])
+
+// should output
+[1, 2]
+[1, 2, 3]
+[3, 4, 5]
+[4, 5]
+[2, 3]
+[1, 2, 8, 9, 5]
+`,
 		LiteralMethods: array_methods,
 		DefaultMethods: default_methods}
 	create_doc("docs/templates/literal.md", "docs/content/docs/literals/array.md", tempData)
 
 	tempData = templateData{
-		Title:          "Hash",
-		Example:        `people = [{"name": "Anna", "age": 24}, {"name": "Bob", "age": 99}];`,
+		Title: "Hash",
+		Example: `people = [{"name": "Anna", "age": 24}, {"name": "Bob", "age": 99}];
+
+// reassign of values
+h = {"a": 1, 2: true}
+puts(h["a"])
+puts(h[2])
+h["a"] = 3
+h["b"] = "moo"
+puts(h["a"])
+puts(h["b"])
+puts(h[2])h = {"a": 1, 2: true}
+puts(h["a"])
+puts(h[2])
+h["a"] = 3
+h["b"] = "moo"
+
+// should output
+1
+true
+3
+"moo"
+true`,
 		LiteralMethods: hash_methods,
 		DefaultMethods: default_methods}
 	create_doc("docs/templates/literal.md", "docs/content/docs/literals/hash.md", tempData)
