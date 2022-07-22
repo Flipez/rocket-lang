c48f00fdb7c31fb972e12e268031dff759dcc68c
Flipez <code@brauser.io>
2022-07-22 14:17:36 +0200

    implement json to object conversion
    Signed-off-by: Flipez <code@brauser.io>

--- object/json.go
+++ object/json.go
@@ -2,7 +2,6 @@ package object
 
 import (
 	"encoding/json"
-	"fmt"
 )
 
 type JSON struct{}
@@ -23,17 +22,50 @@ func init() {
 				[]string{STRING_OBJ},
 			},
 			method: func(_ Object, args []Object, _ Environment) Object {
-				m := map[string]interface{}{}
+				var i interface{}
 				input := args[0].(*String).Value
 
-				err := json.Unmarshal([]byte(input), &m)
+				err := json.Unmarshal([]byte(input), &i)
+
 				if err != nil {
 					return NewErrorFormat("Error while parsing json: %s", err)
 				}
-				fmt.Println(m)
 
-				return NIL
+				return interfaceToObject(i)
 			},
 		},
 	}
+}
+
+func interfaceToObject(i interface{}) Object {
+	switch v := i.(type) {
+	case map[string]interface{}:
+		jsonObject := NewHash(nil)
+		for key, val := range v {
+			hp := HashPair{
+				Key:   NewString(key),
+				Value: interfaceToObject(val),
+			}
+
+			jsonObject.Pairs[hp.Key.(Hashable).HashKey()] = hp
+		}
+
+		return jsonObject
+	case []interface{}:
+		jsonArray := NewArray(nil)
+		for element := range v {
+			jsonArray.Elements = append(jsonArray.Elements, interfaceToObject(element))
+		}
+		return jsonArray
+	case string:
+		return NewString(v)
+	case float64:
+		return NewFloat(v)
+	case bool:
+		if v {
+			return TRUE
+		}
+		return FALSE
+	}
+	return NIL
 }
