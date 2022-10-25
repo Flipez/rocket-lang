7694aa9fd1ce95f16951ce53572e3b14bb348094
Flipez <code@brauser.io>
2022-10-25 23:28:47 +0200

    try
    Signed-off-by: Flipez <code@brauser.io>

--- evaluator/assign.go
+++ evaluator/assign.go
@@ -18,64 +18,13 @@ func evalAssign(a *ast.Assign, env *object.Environment) (val object.Object) {
 	case *ast.Identifier:
 		env.Set(v.String(), evaluated)
 	case *ast.Index:
-		obj, _ := env.Get(v.Left.String())
-		switch o := obj.(type) {
-		case *object.Array:
-			idx, err := handleIntegerIndex(v, env)
-			if err != nil {
-				return object.NewError(err)
-			}
-
-			if l := int64(len(o.Elements)); int64(math.Abs(float64(idx))) >= l {
-				return object.NewErrorFormat(
-					"index out of range, got %d but array has only %d elements", idx, l,
-				)
-			}
-
-			if idx < 0 {
-				idx = int64(len(o.Elements)) + idx
-			}
-
-			o.Elements[idx] = evaluated
-		case *object.Hash:
-			obj := Eval(v.Index, env)
-			h, ok := obj.(object.Hashable)
-			if !ok {
-				return object.NewErrorFormat("expected index to be hashable")
-			}
-
-			key := h.HashKey()
-			o.Pairs[key] = object.HashPair{Key: obj, Value: evaluated}
-		case *object.String:
-			idx, err := handleIntegerIndex(v, env)
-			if err != nil {
-				return object.NewError(err)
-			}
-
-			if l := int64(len(o.Value)); int64(math.Abs(float64(idx))) >= l {
-				return object.NewErrorFormat(
-					"index out of range, got %d but string is only %d long", idx, l,
-				)
-			}
-
-			if idx < 0 {
-				idx = int64(len(o.Value)) + idx
-			}
-
-			strEval, ok := evaluated.(*object.String)
-			if !ok {
-				return object.NewErrorFormat("expected STRING object, got %s", evaluated.Type())
-			}
-			if l := len(strEval.Value); l != 1 {
-				return object.NewErrorFormat(
-					"expected STRING object to have a length of 1, got %d", l,
-				)
-			}
-
-			o.Value = o.Value[:idx] + strEval.Value + o.Value[idx+1:]
-		default:
-			return object.NewErrorFormat("expected object to be indexable")
+		switch v.Left.(type) {
+		case *ast.Index:
+			return evalAssignIndex(v.Left.(*ast.Index), env, evaluated)
 		}
+
+		return evalAssignIndex(v, env, evaluated)
+
 	}
 	return evaluated
 }
@@ -87,4 +36,67 @@ func handleIntegerIndex(ai *ast.Index, env *object.Environment) (int64, error) {
 		return 0, fmt.Errorf("expected index to be an INTEGER, got %s", obj.Type())
 	}
 	return num.Value, nil
+}
+
+func evalAssignIndex(v *ast.Index, env *object.Environment, evaluated object.Object) (val object.Object) {
+	obj, _ := env.Get(v.Left.String())
+	switch o := obj.(type) {
+	case *object.Array:
+		idx, err := handleIntegerIndex(v, env)
+		if err != nil {
+			return object.NewError(err)
+		}
+
+		if l := int64(len(o.Elements)); int64(math.Abs(float64(idx))) >= l {
+			return object.NewErrorFormat(
+				"index out of range, got %d but array has only %d elements", idx, l,
+			)
+		}
+
+		if idx < 0 {
+			idx = int64(len(o.Elements)) + idx
+		}
+
+		o.Elements[idx] = evaluated
+	case *object.Hash:
+		obj := Eval(v.Index, env)
+		h, ok := obj.(object.Hashable)
+		if !ok {
+			return object.NewErrorFormat("expected index to be hashable")
+		}
+
+		key := h.HashKey()
+		o.Pairs[key] = object.HashPair{Key: obj, Value: evaluated}
+	case *object.String:
+		idx, err := handleIntegerIndex(v, env)
+		if err != nil {
+			return object.NewError(err)
+		}
+
+		if l := int64(len(o.Value)); int64(math.Abs(float64(idx))) >= l {
+			return object.NewErrorFormat(
+				"index out of range, got %d but string is only %d long", idx, l,
+			)
+		}
+
+		if idx < 0 {
+			idx = int64(len(o.Value)) + idx
+		}
+
+		strEval, ok := evaluated.(*object.String)
+		if !ok {
+			return object.NewErrorFormat("expected STRING object, got %s", evaluated.Type())
+		}
+		if l := len(strEval.Value); l != 1 {
+			return object.NewErrorFormat(
+				"expected STRING object to have a length of 1, got %d", l,
+			)
+		}
+
+		o.Value = o.Value[:idx] + strEval.Value + o.Value[idx+1:]
+	default:
+		return object.NewErrorFormat("expected object to be indexable")
+	}
+
+	return evaluated
 }
