4f0d67e511cc4e43d0705060b5dedbd1a2dc7ccc
Flipez <code@brauser.io>
2022-12-20 18:59:23 +0100

    use stringable when possible
    Signed-off-by: Flipez <code@brauser.io>

--- stdlib/puts.go
+++ stdlib/puts.go
@@ -8,7 +8,12 @@ import (
 
 func putsFunction(env object.Environment, args ...object.Object) object.Object {
 	for _, arg := range args {
-		fmt.Println(arg.Inspect())
+		if val, ok := arg.(object.Stringable); ok {
+			fmt.Println("stringable")
+			fmt.Println(val.ToStringObj(nil).Value)
+		} else {
+			fmt.Println(arg.Inspect())
+		}
 	}
 
 	return object.NIL
