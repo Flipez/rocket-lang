e30b2ed5ebf5f9d14abd400ca3329d6c37b1220d
Flipez <code@brauser.io>
2023-09-30 12:34:42 +0300

    bump docusaurus latest version
    Signed-off-by: Flipez <code@brauser.io>

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -32,7 +32,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
             'https://github.com/flipez/rocket-lang/tree/main/docs/',
-            lastVersion: 'v0.21.0',
+            lastVersion: 'v0.22.0',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
